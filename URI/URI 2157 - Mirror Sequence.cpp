405c3012e872509b72a0a5e3a157da8724cee208
SM Iftekhar Alam Tipu <38460302+Iftekhar79@users.noreply.github.com>
2020-03-07 12:20:49 +0600

    Mirror Sequence

---
+++ URI/URI 2157 - Mirror Sequence.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<string>
+
+
+using namespace std;
+
+
+int main(){
+	
+	int a,b;
+	char res[10];
+	int test;
+	
+	
+	cin>>test;
+	
+	
+	
+    while(test--){
+    	
+	cin>>a>>b;
+	
+	for(int i=a;i<=b;i++){
+	
+		cout<<i;		
+		
+   	}
+
+	
+	for(int i=b;i>=a;i--){
+		
+         sprintf(res,"%d",i);
+         
+       
+	   	for(int y=strlen(res)-1;y>=0;y--){
+	        
+				cout<<res[y];
+		
+		
+	      }
+		
+	}
+	
+		
+	cout<<endl;
+	
+  
+  }
+
+
+	
+}
